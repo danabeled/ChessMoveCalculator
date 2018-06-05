244a6fee607475d2b477fa98dbf2f7ea88c35032
Daniel Abel <danabeled@gmail.com>
2018-06-04 22:35:40 -0400

    refactored checking moves to piece where it belongs

--- chessboard.py
+++ chessboard.py
@@ -51,92 +51,21 @@ class ChessBoard:
         for rank in self.ranks:
             for square in rank:
                 if(square != 0 and square.color == color):
-                    moveSum += len(self.removeIllegalMoves(square))
+                    moveSum += len(square.getMoveset(self.ranks))
         return moveSum
     
-    def removeIllegalMoves(self, p):
-        """Removes all illegal moves from a piece's moveset
-        
-        Pieces stored movesets are calculated based on current x, y or 
-        hardcoded. Therefore the board must remove moves that are invalid
-        based on it's knowledge of the board. Moves cannot be made if a
-        piece of the same color is on the square
-        
-        Args:
-            p: piece to get moveset 
-            
-        Returns:
-            Parried down move list for the piece
-        """
-        moveset = p.getMoveset();
-        correctMoveSet = []
-        for move in moveset:
-            logger.debug('Evaluating move %r', move)
-            if(self.outOfBounds(move) or self.occupiedBySameColor(move, p)):
-                logger.debug('Invalid move found %r', move)
-            else:
-                correctMoveSet.append(move)
-        return correctMoveSet       
     
-    def outOfBounds(self, move):
-        """Determines if a move is within the board 
         
-        For some pieces it was easier to make their complete moveset than 
-        calculate in the subclass, since the moves were so limited, this makes 
-        the need to check movesets if it is outside the array
-        
-        Args:
-            move: A move where [0] is the x coordinate and [1] is the y 
-            coordinate
-            
-        Returns:
-            boolean of if within board or not
-        """
-        return move[0] > 8 or move[0] < 1 or move[1] > 8 or move[1] < 1
-    
-    def getSquare(self, x, y):
-        """Fetches the object stored at the x,y coordinate in the ranks array
-        
-        Args:
-            x: File number
-            y: Rank number
-            
-        Returns:
-            Object stored in array at that square, or 0 if nothing stored there
-        
-        For readability, moves are stored with their actual rank/file numbers.
-        These must be translated into array locations.
-        """
-        return self.ranks[8 - y][x - 1] 
-        
-    def occupiedBySameColor(self, move, piece):
-        """Determines if there is a piece of the same color in a sqaure
-        
-        Args:
-            move: An x, y pair where [0] is the x and [1] is the y
-            
-        Returns:
-            0 if no piece, or piece is of other color
-            1 if piece of same color
-        """
-        square = self.getSquare(move[0], move[1])
-        logger.debug('Object for square %r', square)
-        if square == 0:
-            return 0
-        else:            
-            logger.debug('Color for square %r', square.color)
-            logger.debug('Color for piece %r', piece.color)
-            return square.color == piece.color
         
 knight = pieces.Knight(4, 5, 0)
 king = pieces.King(5, 1, 0)
 rook = pieces.Rook(7,7,0)
 bishop = pieces.Bishop(2,2,0)
-print(king.getMoveset())
 board = ChessBoard()
-print("King's moves:", board.removeIllegalMoves(king))
-print("Knights's moves:", board.removeIllegalMoves(knight))
-print("Rook's moves:", board.removeIllegalMoves(rook))
-print("Bishop's moves:", board.removeIllegalMoves(bishop))
+board.display()
+print("King's moves:", king.getMoveset(board.ranks))
+print("Knights's moves:", knight.getMoveset(board.ranks))
+print("Rook's moves:", rook.getMoveset(board.ranks))
+print("Bishop's moves:", bishop.getMoveset(board.ranks))
 board.display()
 print(board.calculateAllMoves(0))
--- pieces.py
+++ pieces.py
@@ -4,21 +4,25 @@ Created on Sun Jun  3 13:12:10 2018
 
 @author: Dan
 """
+import logging
+logger = logging.getLogger('PIECE')
+logger.setLevel(logging.DEBUG)
 
 class Piece:
       
     def __init__(self, current_x, current_y, color):
+        
+        #self.logger.setLevel(logging.ERROR)
+        
         self.x = current_x
         self.y = current_y
         self.color = color
         
-    def getMoveset(self):
-        """
-        This method returns a complete moveset, it does not check the board 
-        for conlicting pieces
-        """
-        print("No piece should get here")
     pieceLetter = "NA"
+    
+    def __getMoveset__(self, ranks):
+        print("Nothing should get here")
+    
     def __str__(self):
         toStrValue = ""
         if(self.color ==  0):
@@ -28,10 +32,93 @@ class Piece:
             
         return toStrValue + chr(ord('a') + self.x - 1) + str(self.y)
     
+    
+    def outOfBounds(self, move):
+        """Determines if a move is within the board 
+        
+        For some pieces it was easier to make their complete moveset than 
+        calculate in the subclass, since the moves were so limited, this makes 
+        the need to check movesets if it is outside the array
+        
+        Args:
+            move: A move where [0] is the x coordinate and [1] is the y 
+            coordinate
+            
+        Returns:
+            boolean of if within board or not
+        """
+        return move[0] > 8 or move[0] < 1 or move[1] > 8 or move[1] < 1
+    
+    
+    
+    def removeIllegalMoves(self, ranks):
+        """Removes all illegal moves from a piece's moveset
+        
+        Pieces stored movesets are calculated based on current x, y or 
+        hardcoded. Therefore if hardcoded the board must remove moves that 
+        are invalid based on it's knowledge of the board. Moves cannot be 
+        made if a piece of the same color is on the square.
+        May be able to remove this once we abstract more logic into the pieces
+        
+        Args:
+            p: piece to get moveset 
+            
+        Returns:
+            Parried down move list for the piece
+        """
+        moveset = self.__getMoveset__(ranks)
+        correctMoveSet = []
+        for move in moveset:
+            logger.debug('Evaluating move %r', move)
+            if(self.outOfBounds(move) or self.occupiedBySameColor(move, ranks)):
+                logger.debug('Invalid move found %r', move)
+            else:
+                correctMoveSet.append(move)
+        return correctMoveSet       
+    
+    
+    def occupiedBySameColor(self, move, ranks):
+        """Determines if there is a piece of the same color in a sqaure
+        
+        Args:
+            move: An x, y pair where [0] is the x and [1] is the y
+            
+        Returns:
+            0 if no piece, or piece is of other color
+            1 if piece of same color
+        """
+        square = self.getSquare(move[0], move[1], ranks)
+        logger.debug('Object for square %r', square)
+        if square == 0:
+            return 0
+        else:            
+            logger.debug('Color for square %r', square.color)
+            logger.debug('Color for piece %r', self.color)
+            return square.color == self.color
+        
+            
+    def getSquare(self, x, y, ranks):
+        """Fetches the object stored at the x,y coordinate in the ranks array
+        
+        Args:
+            x: File number
+            y: Rank number
+            
+        Returns:
+            Object stored in array at that square, or 0 if nothing stored there
+        
+        For readability, moves are stored with their actual rank/file numbers.
+        These must be translated into array locations.
+        """
+        return ranks[8 - y][x - 1] 
+        
+    def getMoveset(self, ranks):
+        return self.removeIllegalMoves(ranks)
+    
 class King(Piece):
     pieceLetter = "k"
     
-    def getMoveset(self):
+    def __getMoveset__(self, ranks):
         return [(self.x + 1, self.y),
                 (self.x + 1, self.y + 1),
                 (self.x + 1, self.y - 1),
@@ -39,18 +126,18 @@ class King(Piece):
                 (self.x - 1, self.y + 1),                
                 (self.x - 1, self.y - 1),
                 (self.x, self.y + 1),                
-                (self.x, self.y - 1)]
+                (self.x, self.y - 1)]    
                 
 class Pawn(Piece):
     pieceLetter = "p"
     
-    def getMoveset(self):
+    def __getMoveset__(self, ranks):
         return [(self.x, self.y +1)]
 
 class Rook(Piece):
     pieceLetter = "r"
     
-    def getMoveset(self):
+    def __getMoveset__(self, ranks):
         moves = []
         
         #Add moves to the left
@@ -79,7 +166,7 @@ class Rook(Piece):
 class Bishop(Piece):
     pieceLetter = "b"
     
-    def getMoveset(self):
+    def __getMoveset__(self, ranks):
         moves = []
         
         #Add moves to the left-down
@@ -119,7 +206,7 @@ class Bishop(Piece):
 class Knight(Piece):
     pieceLetter = "n"
     
-    def getMoveset(self):
+    def __getMoveset__(self, ranks):
         return [(self.x - 1, self.y + 2),
                 (self.x + 1, self.y + 2),
                 (self.x + 2, self.y + 1),
@@ -128,3 +215,13 @@ class Knight(Piece):
                 (self.x - 2, self.y - 1),
                 (self.x - 1, self.y - 2),
                 (self.x + 1, self.y - 2)]
+        
+king = King(5, 1, 1)
+print(king.getMoveset([[0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,Rook(7,7,0),0],
+                      [0,0,0,0,0,0,0,0],
+                      [0,0,0,Knight(4, 5, 0),0,0,0,0],
+                      [0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0],
+                      [0,0,0,Pawn(5, 2, 0),Pawn(5, 2, 1),0,0,0],
+                      [0,0,0,0,King(5, 1, 0),0,0,0]]))
