96c924ab41cd1375c66acdd1ae1c3732b53dc069
Daniel Abel <danabeled@gmail.com>
2018-06-16 19:56:38 -0400

    create main class

--- chessboard.py
+++ chessboard.py
@@ -70,21 +70,3 @@ class ChessBoard:
     
         
         
-knight = pieces.Knight(4, 5, 0)
-king = pieces.King(5, 1, 0)
-rook = pieces.Rook(6,7,0)
-bishop = pieces.Bishop(2,2,0)
-queen = pieces.Queen(2,4,0)
-pawn = pieces.Pawn(4, 2, 0)
-board = ChessBoard()
-board.addPieceToBoard("King", 5, 1, "White")
-board.display()
-print("King's moves:", board.getRank(5, 1).getMoveset(board.getRanks()))
-print(repr(knight))
-print("Knights's moves:", knight.getMoveset(board.getRanks()))
-print("Rook's moves:", rook.getMoveset(board.getRanks()))
-print("Bishop's moves:", bishop.getMoveset(board.getRanks()))
-print("Queen's moves:", queen.getMoveset(board.getRanks()))
-print("Pawn's moves: ", pawn.getMoveset(board.getRanks()))
-board.display()
-print(board.calculateAllMoves(0))
