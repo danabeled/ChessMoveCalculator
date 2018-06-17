4ae7764fe1fe89be25ee794b8350be0d38936a6b
Daniel Abel <danabeled@gmail.com>
2018-06-16 20:32:46 -0400

    write bat file for tests and add more tests

--- pieceFactory_test.py
+++ pieceFactory_test.py
@@ -13,8 +13,23 @@ class TestMakingPieces(unittest.TestCase):
     def testKing(self):
         self.assertEqual(str(makePiece("King", 1, 1, 0)), "Ka1")
     
-    def testXIsSet(self):
+    def testXIsSet1(self):
         self.assertEqual(makePiece("King", 1, 1, 0).getX(), 1)
+    
+    def testXIsSet2(self):
+        self.assertEqual(makePiece("King", 2, 1, 0).getX(), 2)
+    
+    def testXIsSet3(self):
+        self.assertEqual(makePiece("King", 3, 1, 0).getX(), 3)
+        
+    def testYIsSet1(self):
+        self.assertEqual(makePiece("King", 1, 1, 0).getY(), 1)
+    
+    def testYIsSet2(self):
+        self.assertEqual(makePiece("King", 1, 2, 0).getY(), 2)
+    
+    def testYIsSet3(self):
+        self.assertEqual(makePiece("King", 1, 3, 0).getY(), 3)
     
     def testWhite(self):
         self.assertEqual(str(makePiece("King", 1, 1, "White"))[0], "K")
---
+++ runTests.bat
@@ -0,0 +1,1 @@
+python -m unittest -v pieceFactory_test.py
